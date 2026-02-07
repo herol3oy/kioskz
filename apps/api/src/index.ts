9523a8378b3e1d9ccc0cf35bfe6709fe5d612983
Hamed Sedighi <hamedsedighi@gmail.com>
2026-02-07 12:51:54 +0100

    add urls endpint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,4 +6,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/urls', (c) => {
+  return c.json([
+    { host: 'huffpost.com', lang: 'en' },
+    { host: 'sapo.pt', lang: 'pt' },
+    { host: 'spiegel.de', lang: 'de' },
+    { host: 'radiofarda.com', lang: 'fa' },
+  ])
+})
+
 export default app
